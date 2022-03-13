99778086c312a94a0a6907892dd5b5259e4224a0
user20112 <devlinpaddock@gmail.com>
2022-03-13 16:38:01 -0400

    Initial commit setup the core, client , and server projects

---
+++ SuperLumic.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SuperLumic.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ SuperLumic.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuperLumic.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
